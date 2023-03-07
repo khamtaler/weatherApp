f19808b8e130b54e56b2472f4c39a7e1952c7aee
Tomasz Sroka <tsroka41@gmail.com>
2023-03-07 18:29:43 +0100

    transfering router to separate location and importing in main

--- src/main.js
+++ src/main.js
@@ -1,24 +1,6 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
-import Home from '@/views/HomeView.vue';
-import About from '@/views/AboutView.vue';
+import router from './router';
 import './assets/tailwind.css';
-
-const router = createRouter({
-	history: createWebHistory(),
-	routes: [
-		{
-			path: '/',
-			name: 'Home',
-			component: Home,
-		},
-		{
-			path: '/about',
-			name: 'About',
-			component: About,
-		},
-	],
-});
 
 createApp(App).use(router).mount('#app');
