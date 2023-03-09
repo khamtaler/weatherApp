991a9bb792b5dddbfa4593fccec81de7279913a4
Tomasz Sroka <tsroka41@gmail.com>
2023-03-09 17:09:26 +0100

    setting up weather codes for icons, switching to another API providing more data, importing icons

--- src/components/AsyncWeather.vue
+++ src/components/AsyncWeather.vue
@@ -8,13 +8,26 @@
 		</div>
 		<div class="mt-5 flex w-full flex-col items-center">
 			<h1 class="text-2xl">{{ route.params.city }}</h1>
-			<h3>{{ new Date().toJSON().slice(0, 10) }}</h3>
-			<p>{{ Math.round(weatherData.main.temp - 272.15) }} &deg;C</p>
-			<p>odczuwalne: {{ Math.round(weatherData.main.feels_like - 272.15) }} &deg;C</p>
-			<img
-				class="h-auto w-[150px]"
-				:src="`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`"
-			/>
+			<h3>
+				{{ weatherData.current_weather.time.split('T')[0] }}
+			</h3>
+			<h3>
+				{{ weatherData.current_weather.time.split('T')[1] }}
+			</h3>
+			<div class="mx-auto mt-5 flex w-full max-w-xl flex-col items-center">
+				<h4>Aktualna pogoda</h4>
+				<div class="mt-5 flex w-full items-center justify-center gap-5">
+					<img
+						:src="`../src/components/icons/${currentWeather[0].svg}.svg`"
+						class="h-auto w-[80px]"
+					/>
+					<div>
+						<h5>{{ currentWeather[0].weather }}</h5>
+						<p>temperatura: {{ Math.round(weatherData.current_weather.temperature) }}&deg;C</p>
+						<p>wiatr: {{ Math.round(weatherData.current_weather.windspeed) }} Km/h</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -22,13 +35,14 @@
 <script setup>
 import axios from 'axios';
 import { useRoute } from 'vue-router';
+import WEATHERCODES from '../constants/WeatherCodes.vue';
 
 const route = useRoute();
 const getWeather = async () => {
 	try {
-		const weather =
-			await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${route.query.lat}&lon=${route.query.long}&appid=f4a0ea94b0cef3995d4266f814856433
-`);
+		const weather = await axios.get(
+			`https://api.open-meteo.com/v1/forecast?latitude=${route.query.lat}&longitude=${route.query.long}&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum&current_weather=true&timezone=Europe%2FBerlin`
+		);
 
 		return weather.data;
 	} catch (err) {
@@ -36,5 +50,12 @@ const getWeather = async () => {
 	}
 };
 const weatherData = await getWeather();
+
+const currentWeather = WEATHERCODES.filter(
+	(item) =>
+		item.from <= weatherData.current_weather.weathercode &&
+		item.to >= weatherData.current_weather.weathercode
+);
+
 console.log(weatherData);
 </script>
